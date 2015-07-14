72a87a662713bc043495be332873177ea336ac3c
NSemakov <nsemakov@yahoo.com>
2015-07-14 17:20:00 +0400

    Initial Commit

---
+++ 6. Data types/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  6. Data types
+//
+//  Created by Admin on 14.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
